60953436f4ca400fbb67c4a83701292bfc22e49c
Dan Chystov <dan.chystov@gmail.com>
2025-03-02 00:58:03 -0500

    Xcode project for Distributor's  mobile  app created

---
+++ mobile-app-customer/PharmaFlowMobileBusiness/PharmaFlowMobileBusiness/PharmaFlowMobileBusinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  PharmaFlowMobileBusinessApp.swift
+//  PharmaFlowMobileBusiness
+//
+//  Created by Dan on 2025-03-02.
+//
+
+import SwiftUI
+
+@main
+struct PharmaFlowMobileBusinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
