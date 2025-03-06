a62556fc43b3f0fe2da17edf9051e8f4859e9074
Andrii Kudenko <andrii.kudenko@gmail.com>
2025-03-06 00:24:30 -0500

    Set up a structure for react web app

--- web-app/business-administration-web-app/src/App.js
+++ web-app/business-administration-web-app/src/App.js
@@ -1,29 +1,40 @@
-import logo from './logo.svg';
-import pf_logo from './pf_logo_white.svg'
-import './App.css';
+
+import pf_logo from './assets/images/pf_logo_white.svg'
+import './assets/styles/App.css'
+import './assets/styles/tailwind.css'
+import { useState } from 'react';
+import LoadingScreen from './components/LoadingScreen';
 
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false)
   return (
-    <Router>
-      <div className='logo'>
-      <img src={pf_logo} alt='logo' height={130}></img>
-      </div>
-      
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link>
-        
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
-  );
+    <>
+      {!isLoaded && <LoadingScreen onFinish={() => setIsLoaded(true)} />}
+      {isLoaded && <HomePage />}
+    </>
+  )
 }
+
+const HomePage = () => (
+  <Router>
+    <div className='logo'>
+    <img src={pf_logo} alt='logo' className='h-30'></img>
+    </div>
+    
+    <nav>
+      <Link to="/" className='text-3xl'>Home</Link> | <Link to="/about">About</Link>
+      
+    </nav>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
